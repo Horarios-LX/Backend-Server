085c66fc01e15129f7c5c9875a4af51d55076681
Movies22 <111647781+Movs22@users.noreply.github.com>
2024-11-30 22:26:25 +0000

    Added endpoints for map editor

--- sandbox.js
+++ sandbox.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const fs = require("fs");
 const cors = require("cors")
+const DB = require("./database")
+
+let stops = new DB("./data/stops.json")
 
 module.exports = (positionCache, date) => {
     router.use(cors({
@@ -32,5 +35,36 @@ module.exports = (positionCache, date) => {
         if (!fs.existsSync("./tripHistory/" + r.params.date + "/trips")) return s.sendStatus(404);
         return s.sendFile("/tripHistory/" + r.params.date + "/trips", { root: "." })
     })
+
+    router.get("/editor/stops/list/:id", async (r, s) => {
+        if(!stops.read(r.params.id)) return s.json([]);
+        return s.json(stops.read(r.params.id))
+    });
+
+    router.get("/editor/stops/upvote/:id/:s", async (r, res) => {
+        if(!stops.read(r.params.id)) return res.sendStatus(404);
+        let s = stops.read(r.params.id);
+        if(!s.find(a => a.si.toString() === r.params.s)) return res.sendStatus(404);
+        if((s.find(a => a.si.toString() === r.params.s).c2 || []).includes(r.query.id)) return res.sendStatus(404);
+        s.find(a => a.si.toString() === r.params.s).c += 1;
+        if(!s.find(a => a.si.toString() === r.params.s).c2) s.find(a => a.si.toString() === r.params.s).c2 = []
+        s.find(a => a.si.toString() === r.params.s).c2.push(r.query.id);
+        stops.write(r.params.id, s);
+        res.sendStatus(200)
+    });
+
+    router.get("/editor/stops/suggest/:id", async (r, res) => {
+        let u = r.query.user;
+        let n = r.query.name;
+        let s = stops.read(r.params.id) || [];
+        s.push({i: r.params.id, name: n, user: u, si: s.length, c: 0, id: r.query.id})
+        stops.write(r.params.id, s);
+        res.sendStatus(200)
+    });
+
+    router.get("/editor/stops/", async (r, res) => {
+        res.json(stops.all())
+    });
+
     return router;
 };
